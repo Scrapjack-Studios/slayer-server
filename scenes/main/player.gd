63fce710d431f3cc6eee393af7c478f97f5561ab
PopeRigby <poperigby@mailbox.org>
2020-10-18 15:15:32 -0700

    Add puppet vars to server

--- scenes/main/player.gd
+++ scenes/main/player.gd
@@ -1,4 +1,11 @@
 extends Node2D
 
+remote var puppet_position
+remote var puppet_movement
+remote var puppet_mouse_position
+remote var puppet_muzzle_position
+remote var puppet_weapon_position
+remote var puppet_weapon_flip
+
 func _physics_process(delta: float) -> void:
 	$"/root/Server".update_position(int(name), position)
--- scenes/main/players.gd
+++ scenes/main/players.gd
@@ -3,4 +3,5 @@ extends Node
 func add_player(id):
 	var player = load("res://scenes/main/player.tscn").instance()
 	player.name = str(id)
+	player.set_network_master(id)
 	GameController.add_child(player)
