7fc494063d73d7e9b2c1455a68e039bce5ba5dc3
PopeRigby <poperigby@mailbox.org>
2020-12-07 15:14:06 -0800

    Fix stupid mistake and world state sync

--- src/main/StateProcessing.gd
+++ src/main/StateProcessing.gd
@@ -3,7 +3,7 @@ extends Node
 var world_state
 
 func _physics_process(delta: float) -> void:
-	if get_parent().player_state_collection.empty():
+	if not get_parent().player_state_collection.empty():
 		world_state = get_parent().player_state_collection.duplicate(true) # makes a deep copy of player states
 		for player in world_state.keys():
 			world_state[player].erase("T") # timestamp isn't needed for this
